68dda2d9b7310973ef39d5674ad4503bb0fdb212
Gustavo Laurindo dos Santos <104037685+lawrindovsk@users.noreply.github.com>
2022-07-07 17:24:25 -0300

    Add files via upload

--- minhoquinha.py
+++ minhoquinha.py
@@ -1,11 +1,15 @@
+###### SNAKE GAME by Gustavo Laurindo (lawrindovsk)
+#Aqui em baixo os imports.
 import pygame, random
 from pygame.locals import *
 
+#Método Grid Random
 def on_grid_random():
     x = random.randint(0,590)
     y = random.randint(0,590)
     return (x//10 * 10, y//10 * 10)
 
+#Método de colisão.
 def collision(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
 
@@ -14,22 +18,28 @@ RIGHT = 1
 DOWN = 2
 LEFT = 3
 
-pygame.init()
-screen = pygame.display.set_mode((400,300))
-pygame.display.set_caption('Snake Game by Gustavo Laurindo (lawrindovsk')
 
+
+#Tela do jogo e nome.
+pygame.init()
+screen = pygame.display.set_mode((600,600))
+pygame.display.set_caption('Snake Game by Gustavo Laurindo (lawrindovsk')
+####################################################
+#Criando a snake/minhoca/cobra
 snake = [(200, 200), (210, 200), (220,200)]
 snake_skin = pygame.Surface((10,10))
 snake_skin.fill((255,255,255))
-
+###################################################
+#Maça e fazendo ela aparecer na tela puxando o grid random.
 apple_pos = on_grid_random()
 apple = pygame.Surface((10,10))
 apple.fill((255,0,0))
-
+######################################
 my_direction = LEFT
 
 clock = pygame.time.Clock()
-
+############################
+#Aqui fica as BINDS(a forma que o jogador vai controlar) , eventos que acontecem com a snake e o update.
 while True:
     clock.tick(10)
     for event in pygame.event.get():
@@ -62,7 +72,7 @@ while True:
     if my_direction == LEFT:
         snake[0] = (snake[0][0] - 10, snake[0][1])
 
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     screen.blit(apple, apple_pos)
     for pos in snake:
         screen.blit(snake_skin,pos)
