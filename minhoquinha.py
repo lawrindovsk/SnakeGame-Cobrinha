6e7d27314d74b882d80c9ee0bd1dfbb8922a92b3
Gustavo Laurindo dos Santos <104037685+lawrindovsk@users.noreply.github.com>
2022-07-10 23:51:10 -0300

    snake/minhoquinha game v.0.2

--- minhoquinha.py
+++ minhoquinha.py
@@ -2,7 +2,8 @@
 #Aqui em baixo os imports.
 import pygame, random
 from pygame.locals import *
-
+pygame.init()
+pygame.font.init()
 
 #Método Grid Random
 def on_grid_random():
@@ -10,9 +11,8 @@ def on_grid_random():
     y = random.randint(0,590)
     return (x//10 * 10, y//10 * 10)
 
-
 #Método de colisão.
-def collision(c1, c2):
+def colisao(c1, c2):
     return (c1[0] == c2[0]) and (c1[1] == c2[1])
 
 UP = 0
@@ -20,64 +20,67 @@ RIGHT = 1
 DOWN = 2
 LEFT = 3
 
-
+pontos = 0
+fonte = pygame.font.SysFont('arial', 40, True, False)
 
 #Tela do jogo e nome.
-pygame.init()
-screen = pygame.display.set_mode((600,600))
-pygame.display.set_caption('Snake Game by Gustavo Laurindo (lawrindovsk)')
+tela = pygame.display.set_mode((600,600))
+pygame.display.set_caption('Snake Game by lawrindovsk')
 ####################################################
 #Criando a snake/minhoca/cobra
-snake = [(200, 200), (210, 200), (220,200)]
-snake_skin = pygame.Surface((10,10))
-snake_skin.fill((255,255,255))
+cobra = [(200, 200), (210, 200), (220,200)]
+cobra_skin = pygame.Surface((10,10))
+cobra_skin.fill((0,255,0))
 ###################################################
 #Maça e fazendo ela aparecer na tela puxando o grid random.
-apple_pos = on_grid_random()
-apple = pygame.Surface((10,10))
-apple.fill((255,0,0))
+maca_pos = on_grid_random()
+maca = pygame.Surface((10,10))
+maca.fill((255,0,0))
 ######################################
 my_direction = LEFT
 
-clock = pygame.time.Clock()
+relogio = pygame.time.Clock()
 ############################
 #Aqui fica as BINDS(a forma que o jogador vai controlar) , eventos que acontecem com a snake e o update.
 while True:
-    clock.tick(10)
-    for event in pygame.event.get():
-        if event.type == QUIT:
+    relogio.tick(10)
+    menssagem = f'Pontos: {pontos}'
+    texto_formatado = fonte.render(menssagem, False, (255,255,255))
+    for evento in pygame.event.get():
+        if evento.type == QUIT:
             pygame.quit()
-
-        if event.type == KEYDOWN:
-            if event.key == K_UP:
+        #Comandos de movimento = caso ele aperte o botão bindado , ele executa a ação.
+        if evento.type == KEYDOWN:
+            if evento.key == K_UP:
                 my_direction = UP
-            if event.key == K_DOWN:
+            if evento.key == K_DOWN:
                 my_direction = DOWN
-            if event.key == K_LEFT:
+            if evento.key == K_LEFT:
                 my_direction = LEFT
-            if event.key == K_RIGHT:
+            if evento.key == K_RIGHT:
                 my_direction = RIGHT
 
-    if collision(snake[0], apple_pos):
-        apple_pos = on_grid_random()
-        snake.append((0,0))
+    if colisao(cobra[0], maca_pos):
+        maca_pos = on_grid_random()
+        cobra.append((0,0))
+        pontos = pontos + 1
 
-    for i in range(len(snake) - 1, 0, -1):
-        snake[i] = (snake[i-1][0], snake[i-1][1])
+    for i in range(len(cobra) - 1, 0, -1):
+        cobra[i] = (cobra[i-1][0], cobra[i-1][1])
 
     if my_direction == UP:
-        snake[0] = (snake[0][0], snake[0][1] - 10)
+        cobra[0] = (cobra[0][0], cobra[0][1] - 10)
     if my_direction == DOWN:
-        snake[0] = (snake[0][0], snake[0][1] + 10)
+        cobra[0] = (cobra[0][0], cobra[0][1] + 10)
     if my_direction == RIGHT:
-        snake[0] = (snake[0][0] + 10, snake[0][1])
+        cobra[0] = (cobra[0][0] + 10, cobra[0][1])
     if my_direction == LEFT:
-        snake[0] = (snake[0][0] - 10, snake[0][1])
+        cobra[0] = (cobra[0][0] - 10, cobra[0][1])
 
-    screen.fill((0, 0, 0))
-    screen.blit(apple, apple_pos)
-    for pos in snake:
-        screen.blit(snake_skin, pos)
+    tela.fill((0, 0, 0))
+    tela.blit(maca, maca_pos)
+    for pos in cobra:
+        tela.blit(cobra_skin, pos)
 
-
+        tela.blit(texto_formatado, (390,20))
     pygame.display.update()
